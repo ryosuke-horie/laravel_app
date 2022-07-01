0f9cd991e515fc6889071b740352d6f3bbe56b1c
EC2 Default User <ec2-user@ip-172-31-24-252.us-west-1.compute.internal>
2022-07-01 01:49:41 +0000

    正規表現ルートと名前付きルート

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,10 +6,14 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index() {
+    public function index($id) {
         $data = [
-            'msg' => 'this is sample message.'
+            'msg' => 'id = ' . $id,
         ];
         return view('hello.index', $data);
+    }
+    
+    public function other() {
+        return redirect()->route('hello');
     }
 }
